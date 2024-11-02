6a43844bd02c0e97bbca074b0431c5ae63a4b47e
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-02 12:45:46 +0330

    edit tags

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-500 min-h-screen w-screen p-5 ">
+        <p className='bg-white text-red-600 p-4 rounded  list-item list-inside' >hi</p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
