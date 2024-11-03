c9794e0657edc62b50110fdf71cc3d5a5fa2c280
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-03 16:39:28 +0330

    added dark mode button

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BiMoon, BiSun } from 'react-icons/bi'
 
 function Layout({ children }) {
+    const [dark, setIsDark] = useState(false)
+
+    const darkHandler = () => {
+        setIsDark(!dark);
+        if (!dark) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }
     return (
-        <div >
+        <div className='' >
             <header className='flex justify-between p-2 bg-blue-600 text-white items-center'>
-                <p className='text-lg'>Crypto News</p>
-                <a href="#" className='bg-white rounded m-2 p-1 px-2 text-blue-700 hover:bg-blue-100 transition-all ease-in-out'>Github</a>
+                <p className='text-lg'>Crypto </p>
+                <div className='flex items-center'>
+                    <button onClick={darkHandler} className='m-2 text-3xl'>
+                        {
+                            dark
+                                ?
+                                <BiSun className='' />
+                                :
+                                <BiMoon className='' />
+                        }
+                    </button>
+                    <a href="#" className='bg-white rounded m-2 p-1 px-2 text-blue-700 hover:bg-blue-100 transition-all ease-in-out'>Github</a>
+                </div>
             </header>
             {children}
             <footer className='p-2 bg-blue-600'>
