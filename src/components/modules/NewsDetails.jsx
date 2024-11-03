3483982019659555ab4930451dbdaf4e206bb389
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-03 16:37:30 +0330

    create news detail page

---
+++ src/components/modules/NewsDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SwiperSlide } from 'swiper/react'
+
+function NewsDetails({ data }) {
+    const { author, description, image_url, title, published_utc } = data
+
+    console.log(data)
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default NewsDetails
