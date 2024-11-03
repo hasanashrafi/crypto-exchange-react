dacbadae6f37a12f2d49167c4d4970f1dc49c60f
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-03 16:38:18 +0330

    updated thead

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import TableRow from './TableRow';
 
 function TableCoin({ coins }) {
-    console.log(coins);
+   
     return (
         <table className="min-w-full ">
             <thead className=''>
-                <tr className="bg-gray-200  ">
+                <tr className="bg-gray-300 rounded ">
                     <th className="p-4  text-gray-600 ">Name</th>
+                    <th className="p-4  text-gray-600">Symbol</th>
                     <th className="p-4  text-gray-600">Price</th>
                     <th className="p-4  text-gray-600">Market Cap</th>
                     <th className="p-4  text-gray-600">24h Change</th>
+                    <th className="p-4  text-gray-600">Chart</th>
                 </tr>
             </thead>
             <tbody className=''>
