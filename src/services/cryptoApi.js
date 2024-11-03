50ac6516f55412f7d6adebf664a6bf316d54478f
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-03 16:39:44 +0330

    remove api key

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,8 @@
 const api_key = "CG-JbJLb9BfQY1ZGzBHXgjzsnuk"
-
+const BASE_URL = "https://api.coingecko.com/api/v3"
 
 const getCoinList = () => {
-    return ` https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en&x_cg_demo_api_key=${api_key}`
+    return `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en&x_cg_demo_api_key=${api_key}`
 }
 
-export { api_key, getCoinList }
+export { getCoinList }
