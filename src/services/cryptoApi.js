c5bfe0e0bc8a31880fa7b9a605e1762c9beef5ee
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-04 22:35:29 +0330

    get page and currency from user

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,8 @@
 const api_key = "CG-JbJLb9BfQY1ZGzBHXgjzsnuk"
 const BASE_URL = "https://api.coingecko.com/api/v3"
 
-const getCoinList = () => {
-    return `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en&x_cg_demo_api_key=${api_key}`
+const getCoinList = (page,currency) => {
+    return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&sparkline=false&locale=en&x_cg_demo_api_key=${api_key}`
 }
 
 export { getCoinList }
