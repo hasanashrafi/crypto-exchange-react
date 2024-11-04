24eff20109a50c48a2ff5e6edde3e79679010caf
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-04 15:51:45 +0330

    responsive slider and remove button

--- src/components/NewsPage.jsx
+++ src/components/NewsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import Loader from './modules/Loader';
 
 function NewsPage() {
     const [news, setNews] = useState([]);
@@ -15,19 +16,38 @@ function NewsPage() {
     }, []);
 
     return (
-        <div className='w-full flex items-center h-96'>
-            <Swiper spaceBetween={200} slidesPerView={2}>
-                {news && news.map((i) => (
-                    <SwiperSlide key={i.id}>
-                        <div className='w-96 p-4 border rounded-lg shadow-lg '>
-                            <p className='text-lg font-bold text-black text-wrap my-2'>{i.title}</p>
-                            <img src={i.image_url} className='w-full h-56 rounded-md' alt={i.title} />
-                            <button className='mt-2 bg-green-500 text-white py-1 px-3 rounded'>Read More</button>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
+        <>
+            <p className='text-center text-black font-extrabold text-2xl dark:text-white'>Crypto News</p>
+            <div className=' my-5 p-2  w-full flex items-center h-96 dark:text-white'>
+                {news?.length ? (
+
+                    <Swiper
+                        spaceBetween={10}
+                        slidesPerView={1} // Default for small screens
+                        breakpoints={{
+                            640: {
+                                slidesPerView: 2, // For medium screens
+                            },
+                            768: {
+                                slidesPerView: 3, // For larger screens
+                            },
+                        }}>
+                        {news && news.map((i) => (
+                            <SwiperSlide key={i.id} style={{ width: "318px" }}
+                                className='responsive-slide'>
+                                <div className='w-full h-80 overflow-hidden p-4 border rounded-lg shadow-lg '>
+                                    <p className='text-sm font-semibold dark:text-white text-black text-wrap my-2'>{i.title}</p>
+                                    <img src={i.image_url} className=' w-full h-48   rounded-md' alt={i.title} />
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>) : (
+                    <div className='flex items-center justify-center  w-full  text-center'>
+                        <Loader />
+                    </div>
+                )}
+            </div>
+        </>
     );
 }
 
