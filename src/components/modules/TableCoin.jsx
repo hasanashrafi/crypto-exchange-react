747b5cd1fadcfb62bb9f110603e27a846f564ba2
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-04 15:53:17 +0330

    responsive table for small screens

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -2,29 +2,26 @@ import React from 'react';
 import TableRow from './TableRow';
 
 function TableCoin({ coins }) {
-   
     return (
-        <table className="min-w-full ">
-            <thead className=''>
-                <tr className="bg-gray-300 rounded ">
-                    <th className="p-4  text-gray-600 ">Name</th>
-                    <th className="p-4  text-gray-600">Symbol</th>
-                    <th className="p-4  text-gray-600">Price</th>
-                    <th className="p-4  text-gray-600">Market Cap</th>
-                    <th className="p-4  text-gray-600">24h Change</th>
-                    <th className="p-4  text-gray-600">Chart</th>
-                </tr>
-            </thead>
-            <tbody className=''>
-                {
-                    coins.map((coin) =>
-                        <TableRow
-                            key={coin.id}
-                            coin={coin} />
-                    )
-                }
-            </tbody>
-        </table>
+        <div className="overflow-x-auto">
+            <table className="min-w-full">
+                <thead>
+                    <tr className="bg-gray-300 rounded">
+                        <th className="p-4 text-gray-600">Name</th>
+                        <th className="p-4 text-gray-600">Symbol</th>
+                        <th className="p-4 text-gray-600">Price</th>
+                        <th className="p-4 text-gray-600">Market Cap</th>
+                        <th className="p-4 text-gray-600">24h Change</th>
+                        <th className="p-4 text-gray-600">Chart</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {coins.map((coin) => (
+                        <TableRow key={coin.id} coin={coin} />
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
