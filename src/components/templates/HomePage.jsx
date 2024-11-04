4f7e0efa8a632d0b45fdf99408706824fca42979
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-04 16:08:27 +0330

    "Updated HomePage component: added semicolons, changed initial loading state, added try-catch block for data fetch, and reformatted code."

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import TableCoin from '../modules/TableCoin';
 import { getCoinList } from '../../services/cryptoApi';
 import Swipper from '../modules/Swipper';
@@ -6,39 +6,39 @@ import NewsPage from '../NewsPage';
 import Loader from '../modules/Loader';
 
 function HomePage() {
-    const [coins, setCoins] = useState([])
-    const [loading, setLoading] = useState(false)
+    const [coins, setCoins] = useState([]);
+    const [loading, setLoading] = useState(true); // Set initial loading state to true
+
     useEffect(() => {
         const getData = async () => {
-            const response = await fetch(getCoinList())
-            const data = await response.json()
-            setCoins(data)
-            setLoading((loading) => !loading)
-        }
-        getData()
-    }, [])
+            try {
+                const response = await fetch(getCoinList());
+                const data = await response.json();
+                setCoins(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            } finally {
+                setLoading(false); // Ensure loading is set to false after data fetch
+            }
+        };
+        getData();
+    }, []);
 
     return (
-        <div className=' mx-auto p-2'>
-
+        <div className='mx-auto p-2'>
             {loading ? (
-                <div className='flex items-center justify-center h-screen w-full  text-center'>
+                <div className='flex items-center justify-center h-screen w-full text-center'>
                     <Loader />
                 </div>
-            ) :
-                (
-                    <>
-
-                        <NewsPage />
-                        <Swipper coins={coins} />
-                        <TableCoin coins={coins} />
-
-                    </>
-                )}
-
-
+            ) : (
+                <>
+                    <NewsPage />
+                    <Swipper coins={coins} />
+                    <TableCoin coins={coins} />
+                </>
+            )}
         </div>
-    )
+    );
 }
 
-export default HomePage
+export default HomePage;
