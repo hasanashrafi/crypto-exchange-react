d959bdd676f2d906c0072df0d86787f1ba474dfc
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-06 17:01:17 +0330

    create chart state and edit loading state

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 import TableCoin from '../modules/TableCoin';
 import { getCoinList } from '../../services/cryptoApi';
 import Swipper from '../modules/Swipper';
@@ -6,28 +7,30 @@ import NewsPage from '../NewsPage';
 import Loader from '../modules/Loader';
 import Pagination from '../modules/Pagination';
 import Search from '../modules/Search';
+import Chart from '../modules/Chart';
 
 function HomePage() {
     const [coins, setCoins] = useState([]);
-    const [loading, setLoading] = useState(true); // Set initial loading state to true
-    const [page, setPage] = useState(1)
-    const [currency, setCurrency] = useState('usd')
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [currency, setCurrency] = useState('usd');
+    const [chart, setChart] = useState(null);
 
     useEffect(() => {
-        setLoading(true)
+        setLoading(true);
         const getData = async () => {
             try {
-                const response = await fetch(getCoinList(page,currency));
+                const response = await fetch(getCoinList(page, currency));
                 const data = await response.json();
                 setCoins(data);
             } catch (error) {
                 console.error("Error fetching data:", error);
             } finally {
-                setLoading(false); // Ensure loading is set to false after data fetch
+                setLoading(false);
             }
         };
         getData();
-    }, [page,currency]);
+    }, [page, currency]);
 
     return (
         <div className='mx-auto p-2'>
@@ -36,13 +39,14 @@ function HomePage() {
                     <Loader />
                 </div>
             ) : (
-                <>
+                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.6 }}>
+                    <Search currency={currency} setCurrency={setCurrency} />
                     <NewsPage />
                     <Swipper coins={coins} />
-                    <Search currency={currency} setCurrency={setCurrency} />
-                    <TableCoin coins={coins} currency={currency} />
+                    {!!chart && <Chart chart={chart} setChart={setChart} />}
+                    <TableCoin coins={coins} currency={currency} setChart={setChart} />
                     <Pagination page={page} setPage={setPage} />
-                </>
+                </motion.div>
             )}
         </div>
     );
